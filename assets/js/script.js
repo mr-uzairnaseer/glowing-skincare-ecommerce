e1f60de98ef74dfbda91cafa71aceebbc38ab12a
uzair@github <mr.uzairnaseerahmed@gmail.com>
2026-04-23 21:27:12 +0500

    all done

--- assets/js/script.js
+++ assets/js/script.js
@@ -47,16 +47,16 @@ addEventOnElem(navbarLinks, "click", closeNavbar);
  * header sticky & back top btn active
  */
 
-const header = document.querySelector("[data-header]");
+const header = document.querySelector(".header");
 const backTopBtn = document.querySelector("[data-back-top-btn]");
 
 const headerActive = function () {
-  if (window.scrollY > 150) {
+  if (window.scrollY > 80) {
     header.classList.add("active");
-    backTopBtn.classList.add("active");
+    if (backTopBtn) backTopBtn.classList.add("active");
   } else {
     header.classList.remove("active");
-    backTopBtn.classList.remove("active");
+    if (backTopBtn) backTopBtn.classList.remove("active");
   }
 }
 
